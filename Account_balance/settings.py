803bd45fd18eaeeda2233cf4daaa7fc17eca7d5f
AmirHossein Esmaeili <69587936+AmrhsnEs@users.noreply.github.com>
2024-08-18 17:41:23 +0330

    setup_database_connection

--- Account_balance/settings.py
+++ Account_balance/settings.py
@@ -82,8 +82,11 @@ WSGI_APPLICATION = "Account_balance.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'account_blnce',
+        'USER' : 'postgres',
+        'PASSWORD' : '137919',
+        'HOST' : 'localhost',
     }
 }
 
