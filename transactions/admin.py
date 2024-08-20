ec8e606f5094e4f6bf2da1f58395f49636c2f039
AmirHossein Esmaeili <69587936+AmrhsnEs@users.noreply.github.com>
2024-08-20 17:34:01 +0330

    little_changes

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Category,Transactions,Balance
+from .models import Category,Transactions
 
 admin.site.register(Category)
 
 admin.site.register(Transactions)
-admin.site.register(Balance)
